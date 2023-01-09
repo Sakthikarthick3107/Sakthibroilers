bf52a6b6ad345d4a772e0b11f94a5f6e5d7ac5f1
Sakthikarthick3107 <sakthikarthick3107@gmail.com>
2023-01-09 21:53:39 +0530

    Added new files

--- theme.js
+++ theme.js
@@ -2,9 +2,9 @@ var icon=document.getElementById("ikon");
 icon.onclick=function(){
 document.body.classList.toggle("dark-theme");
 if(document.body.classList.contains("dark-theme")){
-    icon.src="sun.png";
+    icon.src="images/sun.png";
     }
 else{
-    icon.src="moon.ico";
+    icon.src="images/moon.ico";
     }
 }
